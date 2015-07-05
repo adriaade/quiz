cad36160c8ca4d2091028761120abbb1ddbda346
adriaade <adriaade@gmail.com>
2015-07-05 14:01:15 +0200

    Lista de preguntas(b)

--- models/models.js
+++ models/models.js
@@ -35,7 +35,7 @@ exports.Quiz = Quiz; // exportar tabla Quiz
 sequelize.sync().then(function () {
 	// then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function (count) {
-		if (count === 0) { // la tabla se inicializa solo si está vacía
+		if (count === 1) { // la tabla se inicializa solo si está vacía
 		 Quiz.bulkCreate( 
         [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
           {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,9 @@ router.get('/autor', function(req, res) {
 res.render('autor');
 });
 
-+// Definición de rutas de /quizes
-+router.get('/quizes',                      quizController.index);
-+router.get('/quizes/:quizId(\\d+)',        quizController.show);
-+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
