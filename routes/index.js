0bcf6622322c3cf458a2ce54fecc828ba2ad4b40
adriaade <adriaade@gmail.com>
2015-07-05 14:29:49 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,9 @@ router.get('/autor', function(req, res) {
 res.render('autor');
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
