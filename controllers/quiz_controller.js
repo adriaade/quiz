b13cafd2e59257406216353b872bc805815021be
adriaade <adriaade@gmail.com>
2015-07-14 18:55:25 +0200

    Crear preguntas y validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -35,7 +35,8 @@ exports.index = function (req, res) {
 // GET /quizes/:id
 exports.show = function (req, res) {
 	res.render('quizes/show', {
-		quiz: req.quiz
+		quiz: req.quiz,
+		errors: []
 	});
 };
 
@@ -47,6 +48,45 @@ exports.answer = function (req, res) {
 	}
 	res.render('quizes/answer', {
 		quiz: req.quiz,
-		respuesta: resultado
+		respuesta: resultado,
+		errors: []
 	});
+};
+
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build({ // crea objeto quiz
+		pregunta: "Pregunta",
+		respuesta: "Respuesta"
+	});
+	res.render('quizes/new', {
+		quiz: quiz,
+		errors: []
+	});
+};
+
+// POST /quizes/create
+exports.create = function (req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	quiz
+		.validate()
+		.then(
+			function (err) {
+				if (err) {
+					res.render('quizes/new', {
+						quiz: quiz,
+						errors: err.errors
+					});
+				} else {
+					quiz // save: guarda en DB campos pregunta y respuesta de quiz
+						.save({
+							fields: ["pregunta", "respuesta"]
+						})
+						.then(function () {
+							res.redirect('/quizes')
+						})
+				} // res.redirect: Redirección HTTP a lista de preguntas
+			}
+		);
 };
