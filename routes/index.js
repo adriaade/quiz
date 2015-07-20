5e786c29018fca7614d399e8a236637c0707ad03
adriaade <adriaade@gmail.com>
2015-07-20 19:29:32 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get('/autor', function (req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
@@ -37,7 +38,10 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+	sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
